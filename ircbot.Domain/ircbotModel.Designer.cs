d264e4e1056a5c33f25b846481647f01026eb842
Jason Hooper <nirgle@gmail.com>
2015-07-26 00:59:00 -0400

    EDMX objects re-org

--- ircbot.Domain/ircbotModel.Designer.cs
+++ ircbot.Domain/ircbotModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\nirgledev\Documents\Visual Studio 2015\Projects\ircbot\ircbot.Domain\ircbotModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Jason\Documents\irception.org\ircbot.Domain\ircbotModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
